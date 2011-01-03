70002b13a18427b728237882c4dc9797465ba5cd
Michal Zochniak <michal@zapu.net>
2011-01-03 10:38:58 +0100

    Basic functionality in EventTablePage.

---
+++ src/plugins/FreePublisher/ui/EventTablePage.java
@@ -0,0 +1,192 @@
+package plugins.FreePublisher.ui;
+
+import freenet.pluginmanager.PluginRespirator;
+import freenet.support.HTMLNode;
+import freenet.support.api.HTTPRequest;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import plugins.FreePublisher.events.EventTable;
+import plugins.FreePublisher.models.EventTableModel;
+
+/**
+ *
+ * @author zapu
+ */
+public class EventTablePage extends WebPage
+{
+    private EventTableModel eventTableModel;
+
+    public EventTablePage(PluginRespirator pr, EventTableModel eventTableModel)
+    {
+        super(pr);
+
+        this.eventTableModel = eventTableModel;
+
+        tables = new HashMap<String, ViewTableAction>();
+
+        registerAction("", new IndexAction());
+        registerAction("open", new OpenTableAction());
+    }
+
+    @Override
+    public String path()
+    {
+        return "/publisher/table";
+    }
+
+    private Map<String, ViewTableAction> tables;
+    private int tableId = 0;
+
+    private void addShowAction(ViewTableAction action)
+    {
+        tables.put(action.getKey(), action);
+        registerAction("show_" + action.getAutoId(), action);
+    }
+
+    private void removeShowAction(ViewTableAction action)
+    {
+        tables.remove(action.getKey());
+        removeAction("show_" + action.getAutoId());
+    }
+
+    class ViewTableAction implements WebPageAction, Runnable
+    {
+        private String key;
+        private Thread thread;
+        private EventTable table;
+        private String lastError;
+        private int autoId;
+
+        public ViewTableAction(String key)
+        {
+            this.key = key;
+            this.autoId = tableId++;
+        }
+
+        public String getKey()
+        {
+            return key;
+        }
+
+        public int getAutoId()
+        {
+            return autoId;
+        }
+
+        public void startFetching()
+        {
+            if(thread != null)
+                return;
+
+            thread = new Thread(this);
+            thread.start();
+        }
+
+        public int handleAction(HTTPRequest request, HTMLNode contentNode, boolean post)
+        {
+            contentNode.addChild("h2", "Action " + autoId + " key: " + key);
+
+            if(request.getParam("req") != null && request.getParam("req").equals("close"))
+            {
+                //Handle this in another action?
+                removeShowAction(this);
+            }
+            else
+            {
+                if(thread != null)
+                {
+                    contentNode.addChild("pre", "working...");
+                }
+                else if(table != null)
+                {
+                    contentNode.addChild("pre", "done...");
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    try
+                    {
+                        table.outputEventTable(stream);
+                        contentNode.addChild("pre", stream.toString());
+                    }
+                    catch(Exception e)
+                    {
+                        System.err.println(e);
+                    }
+                }
+                else
+                {
+                    if(lastError != null)
+                    {
+                        contentNode.addChild("pre", lastError);
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public void run()
+        {
+            try
+            {
+                table = eventTableModel.fetchEventTable(key);
+            }
+            catch(Exception e)
+            {
+                lastError = e.toString();
+            }
+
+            thread = null;
+        }
+    }
+
+    class OpenTableAction implements WebPageAction
+    {
+        public int handleAction(HTTPRequest request, HTMLNode contentNode, boolean post)
+        {
+            if(!post)
+            {
+                contentNode.addChild("pre", "!post");
+                return STATUS_ERROR;
+            }
+
+            if(!request.isPartSet("key"))
+            {
+                contentNode.addChild("pre", "no key set");
+                return STATUS_ERROR;
+            }
+
+            String key = request.getPartAsStringFailsafe("key", 256);
+
+            ViewTableAction action = tables.get(key);
+            if(action == null)
+            {
+                action = new ViewTableAction(key);
+                addShowAction(action);
+                action.startFetching();
+            }
+            
+            //"Redirect" request to action
+            return action.handleAction(request, contentNode, false);
+        }
+
+    }
+
+    class IndexAction implements WebPageAction
+    {
+        public int handleAction(HTTPRequest request, HTMLNode contentNode, boolean post)
+        {
+            HTMLNode form = getPR().addFormChild(contentNode, "", "form name");
+            form.addChild("input",
+                            new String[] { "class", "type", "name" },
+                            new String[] { "config", "text", "key" });
+            form.addChild("input",
+                            new String[] { "type", "name", "value" },
+                            new String[] { "hidden", "action", "open" });
+            form.addChild("input",
+                            new String[] { "type", "value", "style" },
+                            new String[] { "submit", "Show eventTable", "margin-top:10px; margin-bottom:20px;" });
+
+            return STATUS_NOERROR;
+        }
+    }
+}
--- src/plugins/FreePublisher/ui/UserInterface.java
+++ src/plugins/FreePublisher/ui/UserInterface.java
@@ -5,6 +5,7 @@ import freenet.clients.http.PageMaker;
 import freenet.clients.http.ToadletContainer;
 import freenet.pluginmanager.PluginRespirator;
 import plugins.FreePublisher.FreePublisher;
+import plugins.FreePublisher.models.EventTableModel;
 import plugins.FreePublisher.models.IdentityModel;
 
 /**
@@ -19,7 +20,8 @@ public class UserInterface
     private final PageMaker pageMaker;
 
     private MainToadlet mainToadlet;
-    private IdentityPage identityToadlet;
+    private IdentityPage identityPage;
+    private EventTablePage eventTablePage;
 
     public UserInterface(PluginRespirator pr)
     {
@@ -38,18 +40,23 @@ public class UserInterface
         toadletContainer.register(mainToadlet = new MainToadlet(client),
                 "FreePublisher", "/publisher/", true, "Status", "Status", true, null);
 
-        identityToadlet = new IdentityPage(pr, (IdentityModel) publisher.getModel(IdentityModel.class));
+        identityPage = new IdentityPage(pr, (IdentityModel) publisher.getModel(IdentityModel.class));
         
-        toadletContainer.register(identityToadlet, "FreePublisher", "/publisher/identity", true, "Identity", "Identity", true, null);
+        toadletContainer.register(identityPage, "FreePublisher", "/publisher/identity", true, "Identity", "Identity", true, null);
+
+        eventTablePage = new EventTablePage(pr, (EventTableModel) publisher.getModel(EventTableModel.class));
+
+        toadletContainer.register(eventTablePage, "FreePublisher", eventTablePage.path(), true, "Browse", "Browse", true, null);
         
-        toadletContainer.register(FreePublisher.getInstance().taskManager, ""
-                + "FreePublisher", "/publisher/tasks", true, "Tasks", "Tasks", true, null);
+        //toadletContainer.register(FreePublisher.getInstance().taskManager, ""
+        //        + "FreePublisher", "/publisher/tasks", true, "Tasks", "Tasks", true, null);
     }
 
     public void unload()
     {
         toadletContainer.unregister(mainToadlet);
-        toadletContainer.unregister(identityToadlet);
+        toadletContainer.unregister(identityPage);
+        toadletContainer.unregister(eventTablePage);
         toadletContainer.unregister(FreePublisher.getInstance().taskManager);
 
         pageMaker.removeNavigationCategory("FreePublisher");
